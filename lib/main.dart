cac881e33ac317858e299ffa6b9ebc86337f2967
twoskoops707 <Jsmiller31@gmail.com>
2025-09-24 02:57:34 -0700

    Fix Gradle, restore URL launcher, Amazon affiliate, NewsFeed, navigation, CI workflow

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,50 @@ import 'plant_library.dart';
 import 'care_reminders.dart';
 import 'community.dart';
 import 'news_feed.dart';
-void main()=>runApp(const GreenUrbanGrowApp());
-class GreenUrbanGrowApp extends StatelessWidget{ const GreenUrbanGrowApp({super.key}); @override Widget build(BuildContext context){ return MaterialApp(title:'Green Urban Grow',theme:ThemeData(primarySwatch:Colors.green),home:const HomeScreen());}}
-class HomeScreen extends StatefulWidget{ const HomeScreen({super.key}); @override State<HomeScreen> createState()=>_HomeScreenState();}
-class _HomeScreenState extends State<HomeScreen>{
-  int _selectedIndex=0;
-  static const List<Widget> _screens=<Widget>[
-    GardenPlanner(),PlantLibrary(),CareReminders(),Community(),NewsFeed(),
+
+void main() => runApp(GreenUrbanGrow());
+
+class GreenUrbanGrow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Green Urban Grow',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: MainNav(),
+    );
+  }
+}
+
+class MainNav extends StatefulWidget {
+  @override
+  _MainNavState createState() => _MainNavState();
+}
+
+class _MainNavState extends State<MainNav> {
+  int _currentIndex = 0;
+  final _pages = [
+    GardenPlanner(),
+    PlantLibrary(),
+    CareReminders(),
+    Community(),
+    NewsFeed(),
   ];
-  void _onItemTapped(int index){ setState(()=>_selectedIndex=index); }
-  @override Widget build(BuildContext context){
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body:_screens[_selectedIndex],
-      bottomNavigationBar:BottomNavigationBar(
-        type:BottomNavigationBarType.fixed,
-        items:const[
-          BottomNavigationBarItem(icon:Icon(Icons.yard),label:'Planner'),
-          BottomNavigationBarItem(icon:Icon(Icons.local_florist),label:'Library'),
-          BottomNavigationBarItem(icon:Icon(Icons.alarm),label:'Reminders'),
-          BottomNavigationBarItem(icon:Icon(Icons.group),label:'Community'),
-          BottomNavigationBarItem(icon:Icon(Icons.article),label:'News'),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.green,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.nature), label: 'Planner'),
+          BottomNavigationBarItem(icon: Icon(Icons.local_florist), label: 'Library'),
+          BottomNavigationBarItem(icon: Icon(Icons.alarm), label: 'Reminders'),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Community'),
+          BottomNavigationBarItem(icon: Icon(Icons.feed), label: 'News'),
         ],
-        currentIndex:_selectedIndex,
-        selectedItemColor:Colors.green,
-        onTap:_onItemTapped,
       ),
     );
   }
